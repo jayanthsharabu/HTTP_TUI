5c4a6461b63ca6fffb3782aed34816f1509e179c
Jaycepticon <58182966+jayanthsharabu@users.noreply.github.com>
2025-06-30 16:29:54 +0530

    main

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	ui "TUI_HTTP/internal/UI"
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			log.Fatal("Could not open debug log file:", err)
+		}
+		defer f.Close()
+	}
+
+	m := ui.NewModel()
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		log.Fatal("Error running program:", err)
+	}
 }
